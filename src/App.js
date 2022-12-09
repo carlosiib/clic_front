aeb7e83242e3389f7486c2a42690f8240e61bc83
carlosiib <ntic.carlos@gmail.com>
2022-12-09 08:43:50 -0700

    change endpoint url to render

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     let isUnmounted = false;
     async function getData() {
       try {
-        const endpoint = await fetch("https://clic-backend-heroku.herokuapp.com")
+        const endpoint = await fetch("https://clic-backend-render.onrender.com")
         const data = await endpoint.json()
         if (!isUnmounted) {
           setLoading(true)
